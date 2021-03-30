4f59fb004841246c8ff0f16606dff8a886414eda
Jan Boon <jan.boon@kaetemi.be>
2021-03-30 14:06:39 +0800

    Save time

---
+++ templates/class.cpp
@@ -0,0 +1,18 @@
+
+#include "class_name.h"
+
+namespace nmsp {
+
+ClassName::ClassName()
+{
+	// ...
+}
+
+ClassName::~ClassName()
+{
+	// ...
+}
+
+} /* namespace nmsp */
+
+/* end of file */
---
+++ templates/class.h
@@ -0,0 +1,25 @@
+
+#pragma once
+#ifndef NMSP_CLASSNAME_H
+#define NMSP_CLASSNAME_H
+
+#include "platform.h"
+
+namespace nmsp {
+
+class ClassName
+{
+public:
+	ClassName();
+	~ClassName();
+	
+private:
+	// ...
+	
+};
+
+} /* namespace nmsp */
+
+#endif /* #ifndef NMSP_CLASSNAME_H */
+
+/* end of file */
