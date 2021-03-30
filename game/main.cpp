bd7c4de00b9a5b56a3c08f91784a2360ce94dc69
Jan Boon <jan.boon@kaetemi.be>
2021-03-30 15:11:43 +0800

    Create dummy GL context

--- game/main.cpp
+++ game/main.cpp
@@ -41,13 +41,14 @@ This is it.
 
 namespace game {
 
-HINSTANCE g_ModuleHandle;
-HWND g_MainWindow;
-HGLRC g_GLContext;
+HINSTANCE ModuleHandle;
+HWND MainWindow;
+HGLRC MainGlContext;
 
 namespace /* anonymous */ {
 
 std::exception_ptr s_WindowProcException = null;
+HGLRC s_DummyMainGlContext;
 
 void update()
 {
@@ -63,7 +64,7 @@ LRESULT CALLBACK windowProc(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, 
 {
 	try
 	{
-		// GAME_DEBUG_ASSERT(g_MainWindow == hwnd); // Only one window, for now
+		// GAME_DEBUG_ASSERT(MainWindow == hwnd); // Only one window, for now
 		switch(uMsg)
 		{
 		case WM_CREATE:
@@ -94,25 +95,21 @@ LRESULT CALLBACK windowProc(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, 
 			HGLRC hglrc = wglCreateContext(hdc);
 			GAME_IF_THROW_LAST_ERROR(!hglrc);
 			wglMakeCurrent(hdc, hglrc);
-			g_GLContext = hglrc;
+			MainGlContext = hglrc;
 
-			// Initialize GL
-			if (gl3wInit())
-				throw Exception("OpenGL failed to initialize."sv, 1);
-			if (!gl3wIsSupported(4, 6))
-				throw Exception("OpenGL 4.6 is not supported."sv, 1);
 			printf("OpenGL %s, GLSL %s\n", glGetString(GL_VERSION), glGetString(GL_SHADING_LANGUAGE_VERSION));
 
 			break;
 		}
 		case WM_CLOSE:
 		{
-			return 0;
+			// return 0;
+			break;
 		}
 		case WM_DESTROY:
 		{
-			if (g_GLContext)
-				wglDeleteContext(g_GLContext);
+			if (MainGlContext)
+				wglDeleteContext(MainGlContext);
 			PostQuitMessage(0);
 			break;
 		}
@@ -122,7 +119,128 @@ LRESULT CALLBACK windowProc(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, 
 	{
 		s_WindowProcException = std::current_exception();
 	}
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+}
+
+LRESULT CALLBACK dummyWindowProc(_In_ HWND hwnd, _In_ UINT uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam)
+{
+	try
+	{
+		switch(uMsg)
+		{
+		case WM_CREATE:
+		{
+			// Create a dummy context
+			PIXELFORMATDESCRIPTOR pfd = {
+				sizeof(PIXELFORMATDESCRIPTOR),
+				1,
+				PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+				PFD_TYPE_RGBA, 32,
+				0, 0, 0, 0, 0, 0,
+				0,
+				0,
+				0,
+				0, 0, 0, 0,
+				24, 8, 0,
+				PFD_MAIN_PLANE,
+				0,
+				0, 0, 0
+			};
+
+			HDC hdc = GetDC(hwnd);
+			GAME_FINALLY([&]() -> void { ReleaseDC(hwnd, hdc); });
+
+			int format;
+			format = ChoosePixelFormat(hdc, &pfd); 
+			SetPixelFormat(hdc,format, &pfd);
+
+			HGLRC hglrc = wglCreateContext(hdc);
+			GAME_IF_THROW_LAST_ERROR(!hglrc);
+			wglMakeCurrent(hdc, hglrc);
+			s_DummyMainGlContext = hglrc;
+
+			// Initialize GL
+			if (gl3wInit())
+				throw Exception("OpenGL failed to initialize."sv, 1);
+			if (!gl3wIsSupported(4, 6))
+				throw Exception("OpenGL 4.6 is not supported."sv, 1);
+
+			// Create the real window
+			RECT r;
+			SetRect(&r, 0, 0, 1280, 720);
+			AdjustWindowRect(&r, WS_OVERLAPPEDWINDOW, FALSE);
+			MainWindow = CreateWindowW(
+				L"PolyverseGame",
+				L"Game",
+				WS_OVERLAPPEDWINDOW,
+				CW_USEDEFAULT, CW_USEDEFAULT,
+				(r.right - r.left), (r.bottom - r.top),
+				0, NULL, ModuleHandle, 0);
+			GAME_IF_THROW_LAST_ERROR(!MainWindow);
+			GAME_DEBUG_ASSERT(MainGlContext);
+			break;
+		}
+		case WM_CLOSE:
+		{
+			return 0;
+		}
+		case WM_DESTROY:
+		{
+			if (s_DummyMainGlContext)
+			{
+				wglDeleteContext(s_DummyMainGlContext);
+				s_DummyMainGlContext = NULL;
+			}
+			if (MainWindow)
+				ShowWindow(MainWindow, SW_SHOWNORMAL);
+			else
+				PostQuitMessage(0);
+			break;
+		}
+		}
+	}
+	catch (...)
+	{
+		s_WindowProcException = std::current_exception();
+	}
+	return DefWindowProcW(hwnd, uMsg, wParam, lParam);
+}
+
+void registerClass(wchar_t *className, WNDPROC wndProc, HICON hIcon)
+{
+	WNDCLASSW wndClass;
+	wndClass.style = CS_DBLCLKS | CS_OWNDC;
+	wndClass.lpfnWndProc = wndProc;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hInstance = ModuleHandle;
+	wndClass.hIcon = hIcon;
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = className;
+	GAME_IF_THROW_LAST_ERROR(!RegisterClassW(&wndClass));
+}
+
+void createMainWindow(HICON hIcon)
+{
+	registerClass(L"PolyverseGameDummy", dummyWindowProc, hIcon);
+	GAME_FINALLY([&]() -> void { UnregisterClassW(L"PolyverseGameDummy", ModuleHandle); });
+
+	RECT r;
+	SetRect(&r, 0, 0, 640, 480);
+	AdjustWindowRect(&r, WS_OVERLAPPEDWINDOW, FALSE);
+	HWND hwnd = CreateWindowW(
+		L"PolyverseGameDummy",
+		L"OpenGL Context",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		(r.right - r.left), (r.bottom - r.top),
+		0, NULL, ModuleHandle, 0);
+	GAME_IF_THROW_LAST_ERROR(!hwnd);
+	GAME_FINALLY([&]() -> void { DestroyWindow(hwnd); });
+
+	GAME_DEBUG_ASSERT(MainWindow);
 }
 
 int main()
@@ -130,42 +248,21 @@ int main()
 	try
 	{
 		// Get module handle
-		g_ModuleHandle = (HINSTANCE)GetModuleHandle(NULL);
-		GAME_FINALLY([&]() -> void { g_ModuleHandle = NULL; });
+		ModuleHandle = (HINSTANCE)GetModuleHandle(NULL);
+		GAME_FINALLY([&]() -> void { ModuleHandle = NULL; });
 		
 		// Use icon from the executable, if any
 		WCHAR szExePath[MAX_PATH];
 		GetModuleFileNameW(NULL, szExePath, MAX_PATH);
-		HICON hIcon = ExtractIconW(g_ModuleHandle, szExePath, 0);
+		HICON hIcon = ExtractIconW(NULL, szExePath, 0);
 		
 		// Register the window class
-		WNDCLASSW wndClass;
-		wndClass.style = CS_DBLCLKS | CS_OWNDC;
-		wndClass.lpfnWndProc = windowProc;
-		wndClass.cbClsExtra = 0;
-		wndClass.cbWndExtra = 0;
-		wndClass.hInstance = g_ModuleHandle;
-		wndClass.hIcon = hIcon;
-		wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-		wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
-		wndClass.lpszMenuName = NULL;
-		wndClass.lpszClassName = L"PolyverseGame";
-		GAME_IF_THROW_LAST_ERROR(!RegisterClassW(&wndClass));
-		GAME_FINALLY([&]() -> void { UnregisterClassW(L"PolyverseGame", g_ModuleHandle); });
+		registerClass(L"PolyverseGame", windowProc, hIcon);
+		GAME_FINALLY([&]() -> void { UnregisterClassW(L"PolyverseGame", ModuleHandle); });
 		
 		// Create a window
-		RECT r;
-		SetRect(&r, 0, 0, 1280, 720);
-		AdjustWindowRect(&r, WS_OVERLAPPEDWINDOW, FALSE);
-		g_MainWindow = CreateWindowW(
-			L"PolyverseGame",
-			L"Game",
-			WS_OVERLAPPEDWINDOW,
-			CW_USEDEFAULT, CW_USEDEFAULT,
-			(r.right - r.left), (r.bottom - r.top),
-			0, NULL, g_ModuleHandle, 0);
-		GAME_IF_THROW_LAST_ERROR(!g_MainWindow);
-		GAME_FINALLY([&]() -> void { DestroyWindow(g_MainWindow); });
+		createMainWindow(hIcon);
+		GAME_FINALLY([&]() -> void { DestroyWindow(MainWindow); });
 		
 		// Message loop
 		MSG msg = { 0 };
